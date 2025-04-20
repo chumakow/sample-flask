5a9ea18e415b17564b76365cba9a14db498a17e7
chumakow <andrew.chumakov@mail.ru>
2025-04-20 04:08:11 +0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, jsonify
+import openai
+from dotenv import dotenv_values
+
+config = dotenv_values('.env')
+openai.api_key = config["OPENAI_API_KEY"]
+
+client = openai.OpenAI()
+
+def get_response(msg):
+    prompt = f"""
+    Take in the input, and respond with itself back, but adding "yopta" in the end. Here is the input: {msg}
+    """
+    
+    response = client.responses.create(
+        input = prompt,
+        model = 'gpt-4.1'
+    )
+    print((response.output[0].content[0].text))
+    response = response.output[0].content[0].text
+    return response
+
+
+app = Flask(__name__,
+            template_folder='templates',
+            static_url_path='',
+            static_folder='static'  
+)
+
+
+@app.route("/")
+def hello_world():
+    app.logger.info('2')
+    return render_template("index.html")
+
+
+@app.route("/execute", methods=["POST"])    
+def execute():                    
+    input_query = request.form.get("query")       
+    app.logger.info(input_query)
+    app.logger.info('1')
+    llm_response = get_response(input_query)
+    return {"output": llm_response}
+
+
