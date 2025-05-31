f1817d80c030fc23df7e9aaada30282835c66207
chumakow <andrew.chumakov@mail.ru>
2025-06-01 00:14:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -129,7 +129,8 @@ def get_ics_file():
     print(response)
 
     temp_filename = 'temp.ics'
-    os.remove(temp_filename)
+    if os.path.exists('temp_filename')
+        os.remove(temp_filename)
     with open(temp_filename, 'wt') as f:
         f.write(response)
     
